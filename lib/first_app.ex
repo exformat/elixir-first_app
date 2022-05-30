4ceb782467faf6b3851d0270d5cd4becbd1ab393
exformat <exformat1988@gmail.com>
2022-05-30 22:50:34 +0600

    calc PI.. its alive)))

--- lib/first_app.ex
+++ lib/first_app.ex
@@ -13,6 +13,25 @@ defmodule FIRST_APP do
 
   """
   def hello do
-    :world
+    calcPI()
+  end
+
+  def calcPI() do
+    PI = 3 + lineNilacant(2, 0)
+  end
+
+
+  def lineNilacant(start, result) do
+    a = start
+    b = start + 1
+    c = start + 2
+
+    if start < 1_000_000 do
+     result = result + 4 / (a * b * c) - lineNilacant(c, result)
+    else
+      result
+    end
+
+
   end
 end
