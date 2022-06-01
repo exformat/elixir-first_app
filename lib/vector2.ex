a0d8d337b3e746606809ca89e472c2b7c26363b2
exformat <exformat1988@gmail.com>
2022-06-01 18:36:19 +0600

    add more vector2 functions

--- lib/vector2.ex
+++ lib/vector2.ex
@@ -38,10 +38,45 @@ defmodule Vector2 do
     len(sub_vector)
   end
 
+  def crs({vec1, x, y}, {vec2, x1, y1}) do
+    result = x * y1 - y * x1
+  end
+
+  def dot({vec1, x, y}, {vec2, x1, y1}) do
+    result = x * x1 + y * y1
+  end
+
+  def rad({vec1, x, y}) do
+    :math.atan2(y, x)
+  end
+
+  def rad({vec1, x, y}, {vec2, x1, y1}) do
+    a = crs({vec2, x1, y1}, {vec1, x, y})
+    b = dot({vec2, x1, y1}, {vec1, x, y})
+
+    :math.atan2(a, b)
+  end
+
+  def rotate({vec1, x, y}, radians) do
+    cos = :math.cos(radians)
+    sin = :math.sin(radians)
+
+    newX = x * cos - y * sin
+    newY = x * sin + y * cos
+
+    result = {:vector, newX, newY}
+  end
+
+  def rotate_around({vec1, x, y}, {vec2, x1, y1}, radians) do
+    sub_vector = sub({vec1, x, y}, {vec2, x1, y1})
+    rot_vector = rotate(sub_vector, radians)
+
+    result = add(rot_vector, {vec2, x1, y1})
+  end
 
 #hand test functions)))
 def start() do
-  vec1 = {:vec1, 20, 20}
+  vec1 = {:vec1, 20, 0}
   vec2 = {:vec2, 10, 10}
 
   dst(vec1, vec2)
